40d4694b7802fbb4bff1772dcf4a602a1462f05e
Izhar Ali <aliizh94@students.rowan.edu>
2023-04-09 05:59:47 -0400

    Day 25, problem 1

---
+++ tree/isSymmetric.py
@@ -0,0 +1,55 @@
+"""
+Difficulty: Easy
+
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+
+Constraints:
+    * The number of nodes in the tree is in the range [1, 1000].
+    * -100 <= Node.val <= 100
+
+Follow up: Could you solve it both recursively and iteratively?
+"""
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# Solution 1: Recursive
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def isMirror(t1, t2):
+            if not t1 and not t2:
+                return True
+            if not t1 or not t2:
+                return False
+            return (t1.val == t2.val) and isMirror(t1.left, t2.right) and isMirror(t1.right, t2.left)
+        return isMirror(root, root)
+    
+# Solution 2: Iterative
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        queue = [root, root]
+        while queue:
+            t1 = queue.pop(0)
+            t2 = queue.pop(0)
+            if not t1 and not t2:
+                continue
+            if not t1 or not t2:
+                return False
+            if t1.val != t2.val:
+                return False
+            queue.append(t1.left)
+            queue.append(t2.right)
+            queue.append(t1.right)
+            queue.append(t2.left)
+        return True
