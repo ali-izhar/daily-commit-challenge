af81d20f7fe798c50700fe421b4428dc86fcde81
Izhar Ali <aliizh94@students.rowan.edu>
2023-04-10 03:54:47 -0400

    Day 26, problem 1

---
+++ int/addDigits.py
@@ -0,0 +1,42 @@
+"""
+Difficulty: Easy
+
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+Example 1:
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+
+Example 2:
+Input: num = 0
+Output: 0
+
+Constraints:
+    * 0 <= num <= 2^31 - 1
+ 
+Follow up: Could you do it without any loop/recursion in O(1) runtime?
+"""
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        def reduce(z):
+            x, y = z // 10, z % 10
+            return x + y
+        result = reduce(num)
+        while result > 9:
+            result = reduce(result)
+        return result
+
+    
+# Solution: O(1) runtime
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        if num % 9 == 0:
+            return 9
+        return num % 9
